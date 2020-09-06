6dc6587c6eb3064b87076b41f0532016c2e0982f
Hurly77 <cameron.leverett@yahoo.com>
2020-09-06 15:46:32 -0700

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
 // Your code here
+function createEmployeeRecord(array){
+    let employee = {}
+    employee.firstName = array[0]
+    return employee
+}
+
+function createEmployeeRecords(){}
+
+function createTimeInEvent(){}
+
+function createTimeOutEvent(){}
