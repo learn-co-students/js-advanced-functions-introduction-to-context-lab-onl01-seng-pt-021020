7dcf13565176c848b86c44ff00d677c065a301df
StevenDRiggs <sfriggs21@gmail.com>
2020-09-10 07:27:31 -0600

    WIP; committed for device change; check tests for progress

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
-// Your code here
+function createEmployeeRecord(emplData) {
+    return {
+        firstName: emplData[0],
+        familyName: emplData[1],
+        title: emplData[2],
+        payPerHour: emplData[3],
+        timeInEvents: [],
+        timeOutEvents: []
+    }
+}
+
+function createEmployeeRecords(emplsData) {
+    return emplsData.map(createEmployeeRecord)
+}
+
+function createTimeInEvent(emplObj, dateStr) {
+    emplObj.timeInEvents.push({
+        type: 'TimeIn',
+        hour: dateStr.split(' ')[1].slice(0, 1),
+        date: dateStr.split(' ')[0]
+    })
+
+    return emplObj
+}
