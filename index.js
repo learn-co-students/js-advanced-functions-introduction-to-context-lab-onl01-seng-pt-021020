fe6945b63ac7701ea166e4db88116f8fb03d8846
Steven Riggs <sfriggs21@gmail.com>
2020-09-13 00:32:53 +0000

    Done.

--- index.js
+++ index.js
@@ -16,9 +16,44 @@ function createEmployeeRecords(emplsData) {
 function createTimeInEvent(emplObj, dateStr) {
     emplObj.timeInEvents.push({
         type: 'TimeIn',
-        hour: dateStr.split(' ')[1].slice(0, 1),
+        hour: parseInt(dateStr.split(' ')[1]),
         date: dateStr.split(' ')[0]
     })
 
     return emplObj
+}
+
+function createTimeOutEvent(emplObj, dateStr) {
+    emplObj.timeOutEvents.push({
+        type: 'TimeOut',
+        hour: parseInt(dateStr.split(' ')[1]),
+        date: dateStr.split(' ')[0]
+    })
+
+    return emplObj
+}
+
+function hoursWorkedOnDate(emplObj, dateStr) {
+    const timeIn = emplObj.timeInEvents.find(timeInEvent => timeInEvent.date === dateStr).hour
+    const timeOut = emplObj.timeOutEvents.find(timeOutEvent => timeOutEvent.date === dateStr).hour
+    
+    return (timeOut - timeIn) / 100
+}
+
+function wagesEarnedOnDate(emplObj, dateStr) {
+    return hoursWorkedOnDate(emplObj, dateStr) * emplObj.payPerHour
+}
+
+function allWagesFor(emplObj) {
+    const dates = emplObj.timeInEvents.map(timeInEvent => timeInEvent.date)
+    
+    return dates.map(date => wagesEarnedOnDate(emplObj, date)).reduce((total, wage) => total + wage)
+}
+
+function calculatePayroll(emplObjsArr) {
+  return emplObjsArr.map(emplObj => allWagesFor(emplObj)).reduce((total, wages) => total + wages)
+}
+
+function findEmployeeByFirstName(emplObjsArr, firstName) {
+    return emplObjsArr.find(emplObj => emplObj.firstName === firstName)
 }
